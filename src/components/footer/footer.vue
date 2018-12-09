fdc8108d407016d960b05483e199f665c334f4ac
chenxiufang <18333617660@163.com>
2018-12-09 19:53:21 +0800

    add footer

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="footer"></div>
+</template>
+
+<script>
+    export default {
+      name: 'footer',
+      props: {
+        footer: {
+          type: Object,
+          default() {
+            return {}
+          }
+        }
+      }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
